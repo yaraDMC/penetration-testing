e0b05bfd58a40e681bbd11b5a8bb821f1fe81c5c
yurany <miludmc10@gmail.com>
2025-07-22 20:36:14 -0500

    Update auto_gobuster.sh

--- gobuster/scripts/auto_gobuster.sh
+++ gobuster/scripts/auto_gobuster.sh
@@ -5,7 +5,6 @@
 echo "🔍 Iniciando escaneo contra $1"
 gobuster dir -u http://$1 \
 -w $2 \
--x .php,.sql,.bak \
 -o results/scan_$(date +%Y%m%d_%H%M%S).txt
 
 echo "✅ Resultados guardados en /results"
